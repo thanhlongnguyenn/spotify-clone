6292f793453eb46c679690aa75caf9087fc6c2fe
thanhlongnguyenn <nguyenthanhlongacer@gmail.com>
2024-02-27 02:08:48 +1100

    Initial commit

---
+++ components/ListItem.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
+
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+  const router = useRouter();
+
+  const onCLick = () => {
+    // add authen
+    router.push(href);
+  };
+  return (
+    <button
+      className="
+        realtive
+        group
+        flex
+        items-center
+        rounded-md
+        overflow-hidden
+        gap-x-4
+        bg-neutral-100/10
+        hover:bg-neutral-100/20
+        transition
+        pr-4"
+    >
+      <div
+        className="
+        relative
+        min-h-[64px]
+        min-w-[64px]
+        "
+      >
+        <Image className="object-cover" fill src={image} alt="Image" />
+      </div>
+      <p className="font-medium truncate py-5">{name}</p>
+      <div
+        className="
+      absolute
+      transition
+      rounded-full
+      flex
+      items-center
+      justify-center
+      bg-green-500
+      p-4
+      drop-shadow-md
+      right-5
+      "
+      >
+        <FaPlay className="text-black" />
+      </div>
+    </button>
+  );
+};
+
+export default ListItem;
